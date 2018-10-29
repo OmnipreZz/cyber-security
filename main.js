c0c3d352bf9eaa6d9ee3069856ad8f9cb9e6cfa0
Geoffrey <geoffrey.frioli@gmail.com>
2018-10-29 22:11:04 +0100

    start security project

---
+++ main.js
@@ -0,0 +1,18 @@
+/* /////////////////// */
+/* ///////// */
+/* ////////////////////////////////Delay Scroll//////////////////////////////// */
+$(document).ready(function () {
+    $('.js-scrollTo').on('click', function () { // Au clic sur un élément
+        var page = $(this).attr('href');
+        $('html, body').animate({ scrollTop: $(page).offset().top }, 1000); // Go
+        return false;
+    });
+});
+
+/* /////////////////// */
+/* ///////// */
+/* ////////////////////////////////Toggle Menu//////////////////////////////// */
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
